c2d65453e2ad415763a8bb04468efe9b70eb4b9b
Number6406 <6406.number@gmail.com>
2017-12-22 23:06:18 +0100

    adding comments

--- ProjetIHMA/menu.js
+++ ProjetIHMA/menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  /* Tous les états de notre automate */
   var STATES = {
     IDLE : 1,
     MENU_OPEN : 2,
@@ -9,6 +10,7 @@ $(document).ready(function() {
 
   var state = STATES.IDLE;
 
+  /* Calcule la distance euclidienne entre le centre d'un élément du menu et la souris */
   function distance(a,b) {
     ax = a.offset().left + a.outerWidth() / 2;
     ay = a.offset().top + (a.outerHeight() / 2);
@@ -18,8 +20,10 @@ $(document).ready(function() {
     );
   }
 
+  /* Calcul de la taille réelle de la Bubble en fonction de la distance au plus proche point de l'élément à la souris */
   function computeBubbleSize(element, evt) {
     var dist = 0;
+    // Calcul de x et y les plus proches de la souris, qui seront réutilisés dans la fonction
     x = Math.min(
       Math.abs(evt.pageX - (element.offset().left + element.outerWidth())),
       Math.abs(evt.pageX - element.offset().left)
@@ -28,18 +32,18 @@ $(document).ready(function() {
       Math.abs(evt.pageY - (element.offset().top + element.outerHeight())),
       Math.abs(evt.pageY - element.offset().top)
     );
-    if(
+    if (
       evt.pageX >= element.offset().left
       && evt.pageX <= element.offset().left + element.outerWidth()
-    ) {
+    ) { // Si la souris se trouve sur l'axe X du menu
       dist = y
     }
-    else if(
+    else if (
       evt.pageY >= element.offset().top
       && evt.pageY <= element.offset().top + element.outerHeight()
-    ) {
+    ) { // Si la souris se trouve sur l'axe Y du menu
       dist = x
-    } else {
+    } else { // Sinon, en fonction des angles du menu
       dist = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
     }
     return dist + 5;
@@ -72,21 +76,21 @@ $(document).ready(function() {
         if( hvd.offset() && ! (evt.pageX > hvd.offset().left + hvd.width())) {
           hvd.parent().removeClass('open');
         }
-        if( hvd.offset() && (evt.pageX > hvd.offset().left + hvd.width()/2)) {
+        if( hvd.offset() && (evt.pageX > hvd.offset().left + hvd.width()/2)) { // Détection d'entrée dans la partie droite (bubble) du menu
           if(hvd.parent('li').children('ul').length == 0 || hvd.hasClass("fav")) {
             console.log("to bubble");
             state = STATES.BUBBLE;
             break;
           }
         }
-        if(!$(evt.target).parent().parent().hasClass("main-navigation")) {
+        if(!$(evt.target).parent().parent().hasClass("main-navigation")) { // Ouverture du premier élément du menu
           $(evt.target).addClass('hovered');
           $(evt.target).parent('li').addClass('open');
         }
         break;
       case STATES.BUBBLE:
         hvd = $('.hovered');
-        if( hvd.offset() && (evt.pageX <= hvd.offset().left + hvd.width()/2)) {
+        if( hvd.offset() && (evt.pageX <= hvd.offset().left + hvd.width()/2)) { // On quitte le mode bubble (côté gauche du menu)
           console.log('to no bubble');
           $(".bubble").css({
             width: 0,
@@ -102,7 +106,7 @@ $(document).ready(function() {
           closest = $(evt.target);
           dist = 5;
         } else {
-          $(".open>ul>li>a.fav").each(function() {
+          $(".open>ul>li>a.fav").each(function() { // Sélection de l'élément à hover
               if( hvd.offset() && ! (evt.pageX > hvd.offset().left + hvd.width())) {
                 $('.hovered').parent().removeClass('open');
               }
@@ -115,6 +119,7 @@ $(document).ready(function() {
           })
           dist = computeBubbleSize(closest, evt);
         }
+        /* Affichage du hover et ajustement de la taille du bubble */
         $(closest).addClass("hovered");
         $(".bubble").css({
           'width': dist*2,
@@ -125,6 +130,7 @@ $(document).ready(function() {
       default:
 
     }
+    /* Déplacement de la div bubble sous la souris */
     var bubble = $(".bubble");
     $(".bubble").css({
       left: evt.pageX - bubble.width()/2,
@@ -137,7 +143,7 @@ $(document).ready(function() {
       case STATES.MENU_OPEN:
       case STATES.NO_BUBBLE:
       case STATES.BUBBLE:
-      default:
+      default: // Dans tous les cas, on active l'item sélectionné, et on remove toutes les sélections
         selectBubble(evt);
         $("li").removeClass("open");
         $('.hovered').removeClass('hovered');
@@ -149,6 +155,7 @@ $(document).ready(function() {
     })
   });
 
+  /* Empêche le grab and drop des liens et images (évite certains problèmes) */
   $(document).on('mouseover mousedown', 'a, img', function() {
     return false;
   });
